9b0739f5e1ad77a0b48be13729413dd28392526d
Maxime Van Hees <maxime.vanhees99@gmail.com>
2025-01-07 18:57:55 +0100

    fix Stellar bug

--- minting/src/stellar.rs
+++ minting/src/stellar.rs
@@ -1,23 +1,27 @@
 use std::collections::HashMap;
-use stellar_base::PublicKey;
-use stellar_horizon::{
-    api,
-    client::{HorizonClient, HorizonHttpClient},
-    request::{Order, PageRequest},
-};
+// use stellar_base::PublicKey;
+// use stellar_horizon::{
+//     api,
+//     client::{HorizonClient, HorizonHttpClient},
+//     request::{Order, PageRequest},
+// };
+
+use stellar_rs::horizon_client::HorizonClient;
+use stellar_rs::models::Order;
+use stellar_rs::transactions::prelude::TransactionsForAccountRequest;
 
 const TFT_ISSUER: &str = "GBOVQKJYHXRR3DX6NOX2RRYFRCUMSADGDESTDNBDS6CDVLGVESRTAC47";
-const PAGE_LIMIT: u64 = 100;
+const PAGE_LIMIT: u8 = 100;
 
 pub struct Horizon {
-    client: HorizonHttpClient,
+    client: HorizonClient,
 }
 
 impl Horizon {
     /// Create a new horizon instance.
     pub fn new(url: &str) -> Self {
         Self {
-            client: HorizonHttpClient::new_from_str(url).unwrap(),
+            client: HorizonClient::new(url).unwrap(),
         }
     }
 
@@ -26,34 +30,45 @@ impl Horizon {
         mint_map: &mut HashMap<[u8; 32], V>,
         other_mint_map: &mut HashMap<[u8; 32], W>,
     ) {
-        let mut cursor = "".to_string();
+        let mut transactions_request = TransactionsForAccountRequest::new()
+            .set_order(Order::Asc)
+            .expect("Can set order")
+            .set_limit(PAGE_LIMIT)
+            .expect("Can set limit")
+            .set_account_id(TFT_ISSUER)
+            .expect("Can set acccount");
         loop {
-            let req =
-                api::transactions::for_account(&PublicKey::from_account_id(TFT_ISSUER).unwrap())
-                    .with_cursor(&cursor)
-                    .with_limit(PAGE_LIMIT)
-                    .with_order(&Order::Ascending);
-            let (_, resp) = self.client.request(req).await.unwrap();
-            for tx in &resp.records {
-                // TODO:
-                //   - b64 decode memo
-                //   - remove entry from mint map
-                if tx.memo_type != "hash" {
+            let transactions_response = self
+                .client
+                .get_transactions_for_account(&transactions_request)
+                .await
+                .unwrap();
+
+            for tx in transactions_response.embedded().records() {
+                println!("{}", tx.hash());
+                if tx.memo_type() != "MEMO_HASH" {
                     continue;
                 }
-                // infallible
-                if let Some(ref memo) = tx.memo {
+                if let Some(ref memo) = tx.memo() {
                     let mut hash = [0; 32];
                     base64::decode_config_slice(memo, base64::STANDARD, &mut hash).unwrap();
                     mint_map.remove(&hash);
                     other_mint_map.remove(&hash);
                 }
             }
-            if resp.records.len() < PAGE_LIMIT as usize {
+
+            if transactions_response.embedded().records().len() < PAGE_LIMIT as usize {
                 break;
             }
-            // TODO: rework so this clone can go
-            cursor.clone_from(&resp.records[resp.records.len() - 1].paging_token);
+
+            transactions_request = transactions_request
+                .set_cursor(
+                    transactions_response.embedded().records()
+                        [transactions_response.embedded().records().len() - 1]
+                        .paging_token()
+                        .clone(),
+                )
+                .expect("Can set cursor");
         }
     }
 }
